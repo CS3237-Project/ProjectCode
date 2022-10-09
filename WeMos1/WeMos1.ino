00b29443647c289f8ba19d94226c9299ad79418f
siewyangzhi <syangzhi54@gmail.com>
2022-10-10 00:52:50 +0800

    Updated WeMos1.ino to send out the correct Activation Signal message via MQTT Updated the Server on Digital Ocean to access the IMU data from the IMU Implement the IMU code to send out IMU Tilt Angle readings to the Server via MQTT

--- WeMos1/WeMos1.ino
+++ WeMos1/WeMos1.ino
@@ -9,6 +9,7 @@ const char* wifiPassword = "23456789"; // To fill out
 const char* mqttBrokerServer = "192.168.43.153"; //The DigitalOcean IP Address as we hosting the MQTT Broker there. For this test, i set it up using my laptop IP.
 const char* mqttUsername = "Device1";
 const char* mqttPassword = "Password123";
+bool activationStatus;
 
 EspMQTTClient client(
   ssid,
@@ -25,16 +26,13 @@ IRrecv irrecv(irReceiver);
 
 IRAM_ATTR void IRTriggered(){
   if (irrecv.decode(&results)) {
-    client.publish("message/Activation", "Activation Signal");
-    irrecv.resume(); // Receive the next value
+    if (activationStatus == false){
+      client.publish("message/Activation", "Activation Signal On");
+    }else{
+      client.publish("message/Activation", "Activation Signal Off");
+    }
+  irrecv.resume(); // Receive the next value
   }
-}
-
-void setup() {
-    Serial.begin(115200);
-    client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
-    irrecv.enableIRIn(); //Enable the IR receiver
-    attachInterrupt(digitalPinToInterrupt(irReceiver),IRTriggered, RISING);
 }
 
 void onConnectionEstablished()
@@ -42,6 +40,13 @@ void onConnectionEstablished()
   client.subscribe("message/Activation", [](const String & payload) {
     Serial.println(payload);
   });
+}
+
+void setup() {
+    Serial.begin(115200);
+    client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+    irrecv.enableIRIn(); //Enable the IR receiver
+    attachInterrupt(digitalPinToInterrupt(irReceiver),IRTriggered, RISING);
 }
 
 void loop() {
